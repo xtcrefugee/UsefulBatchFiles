9a272fb0993caae07425c6409f001a0dcc53af03
xtcrefugee <mark.olive@live.com>
2025-11-19 01:46:44 +0000

    String change for November 2025 Steam update

--- steamcsspatcher.ps1
+++ steamcsspatcher.ps1
@@ -10,8 +10,8 @@ if (Test-Path $css_path\$css_name) {
   $css_input = Get-Content -Raw $css_path\$css_name
   #
   # Text to replace and replacement text, do not remove spaces!
-  $str_whatsnew = "._17uEBe5Ri8TMsnfELvs8-N{box-sizing:border-box;padding:16px 16px 0px 24px;position:relative;height:324px;overflow:hidden;background-image:linear-gradient(to top, #171d25 0%, #2d333c 80%)}"
-  $str_nowhatsnew = "._17uEBe5Ri8TMsnfELvs8-N{display:none !important;                                                                                                                                         }"
+  $str_whatsnew = "._17uEBe5Ri8TMsnfELvs8-N{box-sizing:border-box;padding-top:16px;padding-bottom:0px;padding-inline-start:24px;padding-inline-end:16px;position:relative;height:324px;overflow:hidden;background-image:linear-gradient(to top, #171d25 0%, #2d333c 80%)}"
+  $str_nowhatsnew = "._17uEBe5Ri8TMsnfELvs8-N{display:none !important;                                                                                                                                                                                                    }"
   $str_addshelf = "._3SkuN_ykQuWGF94fclHdhJ{box-sizing:border-box;display:flex;color:#a9a9a9;font-size:14px;font-weight:100;letter-spacing:1px;transition-property:opacity;transition-duration:.21s;transition-timing-function:ease-in-out}"
   $str_noaddshelf = "._3SkuN_ykQuWGF94fclHdhJ{display:none !important;                                                                                                                                                                      }"
   $str_leftcolumn = "._9sPoVBFyE_vE87mnZJ5aB{flex-shrink:0;display:flex;flex-direction:row;min-width:256px;width:272px;max-width:min( 50%, 100% - 400px );position:relative}"
@@ -33,3 +33,4 @@ if (Test-Path $css_path\$css_name) {
 } else {
   [System.Windows.MessageBox]::Show($css_name+" not found. css name may have changed.","Steam CSS Patcher")
 }
+
